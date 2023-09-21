89d271652c5c489d6e6ca7919067a3d0ead5471d
McUserface <eric.allen@ssd412.org>
2023-09-21 14:20:25 -0700

    9/21

--- src/Notes_2_1.java
+++ src/Notes_2_1.java
@@ -14,7 +14,29 @@ class Student{
     Object class to represent a student at Shorecrest.  This class is a
     template I can use to make instances of Student objects later.
      */
+
     private String name;
+    private int IDNum;
+    private int grade;
+    private boolean kelloggStudent;
+    private double GPA;
+
+    // Student Constructor
+    public Student(String name, int grade, boolean kelloggStudent){
+        this.name = name;
+        this.grade = grade;
+        this.kelloggStudent = kelloggStudent;
+        this.IDNum = 9000233;
+        this.GPA = 4.0;
+
+    }
+
+    // Instance method
+    public void setGPA(double newGPA){
+        this.GPA = newGPA;
+        System.out.print (name + " now has a GPA of " + GPA);
+    }
+
 }
 
 class Client {
@@ -24,6 +46,8 @@ class Client {
     used in one client class.
      */
     public static void main(String[] args){
-        Student person = new Student();
+        Student Steve = new Student("Steve", 11, false);
+        Student Bob = new Student("Bob Q", 42, false);
+        Bob.setGPA(1.0);
     }
 }
