7cc3f719e575e7dc6442091d8764552213080903
McUserface <eric.allen@ssd412.org>
2023-09-27 12:01:37 -0700

    notes 2.4

---
+++ src/Animal.java
@@ -0,0 +1,13 @@
+class Animal {
+    private int age;
+
+    //default constructor is assumed
+
+    public void setAge(int newAge) {
+        age = newAge;
+    }
+
+    public void printAge() {
+        System.out.println(age);
+    }
+}
--- src/Client.java
+++ src/Client.java
@@ -1,13 +1,14 @@
-public static void main(String[] args){
+class Client {
+    public static void main(String[] args) {
         /*
         This is what you will be expected to do for the end of this unit on
         the exam.  If you have the classes written for you, can you create
         instances of the objects and call the methods?
          */
-        Cat kitty = new Cat();
-        Cat gato = new Cat("orange", "thomas o'malley");
-        // Now I want to call mehods on object
-        kitty.printName();
-        System.out.println(gato);
-        // name.method();
+            Animal fozzie = new Animal();
+            fozzie.printAge();
+            fozzie.setAge(20);
+            fozzie.printAge();
+
     }
+}
